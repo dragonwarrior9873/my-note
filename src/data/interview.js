22a69d4a83804432c058ff4d01e60a4b1b37f959
pooh <dragonwarrior9873@gmail.com>
2024-07-31 14:17:08 -0500

    changed

--- src/data/interview.js
+++ src/data/interview.js
@@ -1,4 +1,4 @@
-export const types = ["Interview", "React", "React Hook", "JavaScript", "Blockchain"];
+export const types = ["Interview", "React", "React Hook", "JavaScript", "Blockchain","Cosmos_Go"];
 
 export const interview = [
     {
